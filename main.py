ef06d4cc79f74f38ac286502ff1503f122297e2e
Gawkky <jef.theeuwes@telenet.be>
2024-07-12 12:02:57 +0200

    adding cashfunds and login via env file

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import degiroapi
 import os
+from dotenv import load_dotenv
 
 from degiroapi.product import Product
 from degiroapi.order import Order
 from degiroapi.utils import pretty_json
 
+# Load Environment file
+load_dotenv()
+
 dg = degiroapi.DeGiro()
-dg.login()
+dg.login(os.getenv('DGUSERNAME'), os.getenv('DGPASSWORD'))
+
+cash = dg.getdata(degiroapi.Data.Type.CASHFUNDS)
