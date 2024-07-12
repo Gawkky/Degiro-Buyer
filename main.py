98ced0ddc1c0eb49849a259daeae40542e83188a
Gawkky <jef.theeuwes@telenet.be>
2024-07-12 12:48:12 +0200

    Getting Data from Degiro

--- main.py
+++ main.py
@@ -12,4 +12,8 @@ load_dotenv()
 dg = degiroapi.DeGiro()
 dg.login(os.getenv('DGUSERNAME'), os.getenv('DGPASSWORD'))
 
+#Getting data from Degiro
 cash = dg.getdata(degiroapi.Data.Type.CASHFUNDS)
+IWDA = dg.search_products('IWDA.AS')
+rtprice = dg.real_time_price(Product(IWDA).id, dg.Interval.Type.One_Day)
+
