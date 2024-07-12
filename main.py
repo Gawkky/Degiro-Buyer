d92ebbe912f53a0ae7f3365240d8f4642a803483
Gawkky <jef.theeuwes@telenet.be>
2024-07-12 14:20:39 +0200

    adding calculation pcs to buy

--- main.py
+++ main.py
@@ -12,8 +12,20 @@ load_dotenv()
 dg = degiroapi.DeGiro()
 dg.login(os.getenv('DGUSERNAME'), os.getenv('DGPASSWORD'))
 
+dg_tc = 1
+
 #Getting data from Degiro
 cash = dg.getdata(degiroapi.Data.Type.CASHFUNDS)
 IWDA = dg.search_products('IWDA.AS')
 rtprice = dg.real_time_price(Product(IWDA).id, dg.Interval.Type.One_Day)
 
+# Determin TOB
+tob_rate = 0.0012
+
+if (round(cash*tob_rate, 2)) >= 1300:
+    tob = 1300
+else:
+    tob = round(cash*tob_rate, 2)
+
+inv_ceiling = cash - tob - dg_tc
+pcs = int(inv_ceiling // rtprice)
